c8c05cef6824053cd510a5e3558e31b03624ddb2
Marina <sheremetamaryna@gmail.com>
2022-06-02 01:20:35 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -9,17 +9,17 @@ const DEBOUNCE_DELAY = 300;
      submitBtn: document.querySelector('.search-form_button'),
      
  }
-//  let name = searchQuery.value;
 //  let page = 0;
 //  let perPage = 40;
+//  let name = searchQuery.value
 
-// async function fetchImage (name, page) { 
-//     try{
-//         const response = await axios.get(`
-//         https://pixabay.com/api/?key=27790361-d52fedb5b14fb71941e53259d&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
-//         console.log(response);
-//         return response.data
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
+async function fetchImage (name, page) { 
+    try{
+        const response = await axios.get(`
+        https://pixabay.com/api/?key=27790361-d52fedb5b14fb71941e53259d&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
+        console.log(response);
+        return response.data
+    }catch(error){
+        console.log(error)
+    }
+}
